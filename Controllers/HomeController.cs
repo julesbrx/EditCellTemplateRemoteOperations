80eaaadb286acccf9eab7781fcd12214f38ab6cb
Jules Raffoux <jules.raffoux@bewease.fr>
2022-04-07 11:43:46 +0200

    DataSourceLoadOptions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Data.Helpers;
 using DevExtreme.AspNet.Data.ResponseModel;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace EditCellTemplateRemoteOperations.Controllers;
+
+[ModelBinder(BinderType = typeof(DataSourceLoadOptionsBinder))]
+public class DataSourceLoadOptions : DataSourceLoadOptionsBase { }
+
+public class DataSourceLoadOptionsBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext ctx)
+    {
+        var options = new DataSourceLoadOptions();
+        
+        DataSourceLoadOptionsParser.Parse(options, key => ctx.ValueProvider.GetValue(key).FirstOrDefault());
+        ctx.Result = ModelBindingResult.Success(options);
+        
+        return Task.CompletedTask;
+    }
+}
 
 public class HomeController : Controller
 {
@@ -14,8 +32,5 @@ public class HomeController : Controller
     public IActionResult Index() => View();
 
     [HttpGet, ActionName("data")]
-    public LoadResult Data(DataSourceLoadOptionsBase options)
-    {
-        return DataSourceLoader.Load(_data.GetData(), options);
-    }
+    public LoadResult Data(DataSourceLoadOptions options) => DataSourceLoader.Load(_data.GetData(), options);
 }
