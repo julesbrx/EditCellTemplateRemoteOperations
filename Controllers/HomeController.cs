ab6fea362fa4a1c8383e9a54b7c8fc4965903cb3
Jules Raffoux <jules.raffoux@bewease.fr>
2022-04-07 11:22:20 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EditCellTemplateRemoteOperations.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly DataService _data;
+
+    public HomeController(DataService data) => _data = data;
+
+    [HttpGet]
+    public IActionResult Index() => View();
+
+    [HttpGet, ActionName("data")]
+    public List<ProductDto> Data() => _data.GetData();
+}
---
+++ DataService.cs
@@ -0,0 +1,26 @@
+﻿namespace EditCellTemplateRemoteOperations;
+
+public class DataService
+{
+    private readonly List<ProductDto> _products;
+
+    public DataService()
+    {
+        _products = (
+            from i in Enumerable.Range(0, 1000)
+            select new ProductDto
+            {
+                Id = i,
+                Name = "Product " + i
+            }
+        ).ToList();
+    }
+
+    public List<ProductDto> GetData() => _products;
+}
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
